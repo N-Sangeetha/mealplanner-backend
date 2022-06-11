c82c451a48c0b30f6e612d016b8e0cdcb7e20604
N-Sangeetha <sangeetha@cronj.com>
2022-06-11 16:01:03 +0530

    mongodb atlas integration

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/mealplanner-api', {
+// mongoose.connect('mongodb://127.0.0.1:27017/mealplanner-api', {
+mongoose.connect('mongodb+srv://vercel-admin-user:Bokko@456@cluster0.fz8tz.mongodb.net/?retryWrites=true&w=majority', {
 	useUnifiedTopology: true,
 	useCreateIndex: true,
 	useNewUrlParser: true,

65d9c5863eb96bfd1afad2c951c9c99168aed70e
N-Sangeetha <sangeetha@cronj.com>
2022-06-11 14:32:03 +0530

    vercel config

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
+app.get("/api", (req, res) => {
+res.send("test")
+})
+
 app.use(function (req, res, next) {
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET, PUT, PATCH, POST, DELETE');

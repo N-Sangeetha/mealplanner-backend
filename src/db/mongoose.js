89bc9a7c6cb80c56a13ea40ce40e6507da5dedfc
N-Sangeetha <sangeetha@cronj.com>
2022-06-11 18:40:24 +0530

    mongodb atlas connection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
-
+const username = encodeURIComponent('vercel-admin-user')
+const password = encodeURIComponent('Bokko@456')
+const cluster = encodeURIComponent('cluster0.fz8tz.mongodb.net')
 // mongoose.connect('mongodb://127.0.0.1:27017/mealplanner-api', {
 // mongoose.connect('mongodb+srv://vercel-admin-user:Bokko@456@cluster0.fz8tz.mongodb.net/?retryWrites=true&w=majority', {
 // 	useUnifiedTopology: true,
 // 	useCreateIndex: true,
 // 	useNewUrlParser: true,
 // });
+var uri = `mongodb+srv://${username}:${password}@${cluster}/?retryWrites=true&w=majority`
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb+srv://vercel-admin-user:' + encodeURIComponent('Bokko@456') +'@cluster0.fz8tz.mongodb.net/?retryWrites=true&w=majority', {
+    await mongoose.connect(uri, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
